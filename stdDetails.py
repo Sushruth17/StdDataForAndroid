d07cf843898494321f1834dfa9703958cfe94451
Sushruth17 <sushruth.sushe@gmail.com>
2020-06-19 18:48:14 +0530

    added functions for add student

--- stdDetails.py
+++ stdDetails.py
@@ -43,11 +43,25 @@ def main():
 @app.route('/student_data/<name>')
 def search_student(name):
     print("im inside search student")
-    cursor.execute("select address,parentname,age from studentinfo where name = '{0}'".format(name))
+    cursor.execute("select name,address,parentname,age from studentinfo where name = '{0}'".format(name))
     searched_student = cursor.fetchall()
-    print("searced student====---------->", searched_student)
+    searched_student = {'info': searched_student}
+    print("searched student====---------->", searched_student)
     return str(searched_student)
 
+@app.route('/api/post_some_data', methods=['POST'])
+def get_text_prediction():
+    """
+    predicts requested text whether it is ham or spam
+    :return: json
+    """
+    json = request.get_json()
+    print(json)
+    if len(json['text']) == 0:
+        return jsonify({'error': 'invalid input'})
+
+    cursor.execute("insert into studentinfo(name,address,age,parentname) values()")
+    return jsonify({'Added Suvccessfully'})
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
